1218573d1e5ace206b63ab050ec43ea5cc3a020a
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-02-06 19:25:23 +0100

    added more logging

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -3,13 +3,15 @@
 #include <fstream>
 #include <iostream>
 #include <set>
+#include <cmath>
 #include <boost/lexical_cast.hpp>
 
 using namespace std;
 
 Sudoku::Sudoku()
 {
-    //ctor
+    base = 3; //3x3 Sudoku
+    valid = false;
 }
 
 Sudoku::~Sudoku()
@@ -62,13 +64,28 @@ bool Sudoku::load_from_file(const char file[])
         }
     }
     log.add("Sudoku successfully loaded!");
+    valid = true;
     return true;
 }
 
 bool Sudoku::solve()
 {
+    if (!valid)
+    {
+        log.err("Cannot solve an invalid Sudoku!");
+        return false;
+    }
     log.add("Start to solve Sudoku...");
-    bool solved = false;
+    stringstream sstr; //stream to make up log entries
+    int zeros_begin = is_solved(); //unsolved fields before solving
+    int checks = 0; //number of checks needed
+
+    //switches indicating if a line, coulmn, or square was changed by inserting digits
+    //so it has to be considered again during next solving cycle
+    bool ch_lines[3][3]; //[by][fy]
+    bool ch_columns[3][3]; //[bx][fx] //TODO: init
+    bool ch_squares[3][3]; //[bx][by] //TODO: init
+
     //sets including the digits that are already used in respective category
     set<int> lines[3][3]; //[by][fy]
     set<int> columns[3][3]; //[bx][fx]
@@ -88,57 +105,75 @@ bool Sudoku::solve()
                     squares[bx][by].insert(grid[bx][by][fx][fy]);
                 }
             }
+            ch_lines[by][fy] = true; //initialize ch_lines[][]
+            ch_columns[by][fy] = true; //initialize ch_columns[][]
+            ch_squares[by][fy] = true; //initialize ch_squares[][]
         }
     }
 
+    //go over changed fields and search for possible inputs, input if only one possibility
     for (int n = 1; ; n++)
     {
         stringstream sstr;
         sstr << "Starting cycle " << n << "...";
         log.add(sstr.str());
         bool changed = false;
-        //check lines
+
         for (int by = 0; by < 3; by++)
         {
             for (int fy = 0; fy < 3; fy++)
             {
-                for (int bx = 0; bx < 3; bx++)
-                {
-                    for (int fx = 0; fx < 3; fx++)
+//                if (ch_lines[by][fy] == true) //TODO check only on changed lines, columns, squares... but how???
+//                {
+//                    ch_lines[by][fy] = false;
+                    for (int bx = 0; bx < 3; bx++)
                     {
-                        if (grid[bx][by][fx][fy] == 0)
+                        for (int fx = 0; fx < 3; fx++)
                         {
-                            sstr.str("");
-                            sstr << "Checking for (" << bx << "/" << by << "/" << fx << "/" << fy << ")";
-                            log.add(sstr.str());
-                            vector<int> found; //possible digits
-                            for (int i = 1; i <= 9; i++)
-                            {
-                                if (lines[by][fy].find(i) == lines[by][fy].end() && columns[bx][fx].find(i) == columns[bx][fx].end() && squares[bx][by].find(i) == squares[bx][by].end())
-                                {
-                                    found.push_back(i);
-                                }
-                            }
+//                            if (ch_columns[bx][fx] == true)
+//                            {
+//                                ch_columns[bx][fx] = false;
+//                                cout << "FOUND INSERTED FIELD: " << bx << "/" << by << "/" << fx << "/" << fy << ")" << endl;
 
-                            stringstream sstr;
-                            sstr << " -> Candidates are:";
-                            for (vector<int>::iterator it = found.begin(); it != found.end(); it++)
-                            {
-                                sstr << " " << *it;
-                            }
-                            if (found.size() == 1)
-                            {
-                                grid[bx][by][fx][fy] = found[0];
-                                lines[by][fy].insert(grid[bx][by][fx][fy]);
-                                columns[bx][fx].insert(grid[bx][by][fx][fy]);
-                                squares[bx][by].insert(grid[bx][by][fx][fy]);
-                                changed = true;
-                                sstr << " --> inserted!";
-                            }
-                            log.add(sstr.str());
+                                if (grid[bx][by][fx][fy] == 0)
+                                {
+                                    checks++;
+                                    stringstream sstr;
+                                    sstr << "Checking for (" << bx << "/" << by << "/" << fx << "/" << fy << ")";
+                                    log.add(sstr.str());
+                                    vector<int> found; //possible digits
+                                    for (int i = 1; i <= 9; i++)
+                                    {
+                                        if (lines[by][fy].find(i) == lines[by][fy].end() && columns[bx][fx].find(i) == columns[bx][fx].end() && squares[bx][by].find(i) == squares[bx][by].end())
+                                        {
+                                            found.push_back(i);
+                                        }
+                                    }
+
+                                    sstr.str("");
+                                    sstr << " -> Candidates are:";
+                                    for (vector<int>::iterator it = found.begin(); it != found.end(); it++)
+                                    {
+                                        sstr << " " << *it;
+                                    }
+                                    if (found.size() == 1) //only one possibility --> insert digit
+                                    {
+                                        grid[bx][by][fx][fy] = found[0];
+                                        lines[by][fy].insert(grid[bx][by][fx][fy]);
+                                        columns[bx][fx].insert(grid[bx][by][fx][fy]);
+                                        squares[bx][by].insert(grid[bx][by][fx][fy]);
+                                        changed = true;
+                                        ch_lines[by][fy] = true;
+                                        ch_columns[bx][fx] = true;
+                                        ch_squares[bx][by] = true;
+                                        sstr << " --> inserted!";
+                                    }
+                                    log.add(sstr.str());
+                                }
+                            //}//chcolumns
                         }
                     }
-                }
+                //}//chlines
             }
         }
         sstr.str("");
@@ -147,12 +182,43 @@ bool Sudoku::solve()
         if (changed == false) //no digit entered --> no more digits can be found
             break;
     }
-    log.add("...finisehd solving");
-    return true; //TODO check if solved!
+    log.add("...finished solving");
+    int zeros = is_solved();
+    if (zeros == 0)
+    {
+        sstr.str("");
+        sstr << "--> Sudoku solved completely (solved " << zeros_begin - zeros << " of " << (pow(base, 4)) << " fields, needing " << checks << " checks).";
+        log.add(sstr.str());
+        return true;
+    }
+    else
+    {
+        sstr.str("");
+        sstr << "--> " << zeros << " of " << (pow(base, 4)) << " fields left usolved (solved " << zeros_begin - zeros << " fields).";
+        log.add(sstr.str());
+        return false;
+    }
+}
+
+int Sudoku::is_solved()
+{
+    int cnt = 0; //number of '0' in 'grid[][][][]'
+    for (int by = 0; by < 3; by++)
+        for (int fy = 0; fy < 3; fy++)
+            for (int bx = 0; bx < 3; bx++)
+                for (int fx = 0; fx < 3; fx++)
+                    if (grid[bx][by][fx][fy] == 0)
+                        cnt++;
+    return cnt;
 }
 
 void Sudoku::print()
 {
+    if (!valid)
+    {
+        log.err("Cannot print an invalid Sudoku!");
+        return;
+    }
     cout << endl;
     for (int by = 0; by < 3; by++)
     {
--- Sudoku.h
+++ Sudoku.h
@@ -8,7 +8,9 @@ class Sudoku
 {
     protected:
     private:
+        int base; //NxN Sudoku (n x n x n x n)
         int grid[3][3][3][3]; //[block_x][block_y][field_x][field_y] [bx][by][fx][fy]
+        bool valid; //indicates whether Sudoku in 'grid' is valid
         Log log;
 
     public:
@@ -17,8 +19,8 @@ class Sudoku
 
         bool load_from_file(const char file[]);
         bool solve();
+        int is_solved(); //returns number of fields that are not solved ('0') -- return value: '0' = solved, '>0' = unsolved
         void print();
-        void test();
 };
 
 #endif // SUDOKU_H
